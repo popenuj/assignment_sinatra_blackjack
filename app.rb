9ccdf89306321ce5fc6a9c4d29f06fa2167189d9
Jonathan Popenuck <john.popenuck@gmail.com>
2016-11-17 13:46:03 -0800

    worked on blackjack

--- app.rb
+++ app.rb
@@ -1,22 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'erb'
 require './helpers/blackjack_helper.rb'
+
 helpers BlackjackHelper
+
 enable :sessions
 # Avoid warning: http://stackoverflow.com/a/18047653/5113832
 set :session_secret, '*&(^B234'
 
 get "/" do
 
-  #generate new game (give player money)
+  session["bet"] = 0
+  session["purse"] = 100
+  session["player_cards"] = []
+  session["dealer_cards"] = []
   erb :index
 
 end
 
 get "/blackjack" do
 
-  erb :blackjack
+  deal_card
 
+  erb :blackjack
   # generate new hand (start round)
   # ask for bet
     # if bet is
@@ -30,5 +37,10 @@ get "/blackjack" do
 end
 
 post "/blackjack" do
+
+  bet = params[:bet]
+  session["bet"] = bet
+
+  erb :blackjack
 
 end
--- helpers/blackjack_helper.rb
+++ helpers/blackjack_helper.rb
@@ -1,5 +1,22 @@
 module BlackjackHelper
 
+  def deal_card
+    if session["player_cards"].length == 0
+      2.times do
+        session["player_cards"] << deck
+      end
+    else
+    session["player_cards"] << deck
+    end
+    if session["dealer_cards"].length == 0
+      2.times do
+        session["dealer_cards"] << deck
+      end
+    else
+    session["dealer_cards"] << deck
+    end
+  end
+
   def deck
     [1,2,3,4,5,6,7,8,9,10,11,12,13].product(%w(clubs diamonds hearts spades)).sample
   end
